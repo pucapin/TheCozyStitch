5bfb60b5e98578ebf9e3760400f302f25783bbd3
isabella-guerrero <142193621+pucapin@users.noreply.github.com>
2024-10-09 07:34:46 -0500

    Update index.js

--- crochetmarket-main/html/js/index.js
+++ crochetmarket-main/html/js/index.js
@@ -2,18 +2,29 @@ let products = []
 function parseDataToProducts() {
     for(let i = 0; i < data.length; i++) {
         let map = data[i]
-        let product = new Product(map["price"], map["name"], map["description"], map["size"], map["onstock"], map["creator"], map["image"])
+        let product = new Product(map["name"], map["price"], map["description"], map["size"], map["onstock"], map["creator"], map["image"])
         products.push(product)
     }
 }
-
+//Lleva todos los datos del mapa (los productos)
 function renderAllProducts() {
     let container = document.getElementById("products")
     for(let i = 0; i < 6; i++) {
         let product = products[i]
-        container.innerHTML += product.htmlCard()
+        container.innerHTML += product.htmlCard(i)
     }
-}
+} //Renderiza los productos y los coloca en el div con id products.
+
+function openProduct(pos) {
+    let openProduct = products[pos]
+    window.location = "./product.html?name=" + openProduct.name
+    
+} //Coloca el nombre del producto en el link 
+
+
+parseDataToProducts()
+renderAllProducts()
+
 
 parseDataToProducts()
 renderAllProducts()
